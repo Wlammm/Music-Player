edb409c53ab26055d3ed703dae37763f4345541a
Wille2130 <32479398+Wille2130@users.noreply.github.com>
2019-05-01 21:25:53 +0200

    WIP GUI

--- Music Player/Music Player/Form1.cs
+++ Music Player/Music Player/Form1.cs
@@ -8,10 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Music_Player
 {
 	public partial class Form1 : Form
 	{
+        int mySongLength = 2 * 60 + 42;
+        int myCurrentSongTime = 0;
+
 		public Form1()
 		{
 			InitializeComponent();
@@ -19,12 +23,27 @@ namespace Music_Player
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            myCurrentSongTime = 1 * 60 + 32;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnPlay_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            lblFocus.Focus();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            lblFocus.Focus();
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            lblFocus.Focus();
+        }
+
+        private void barTimeline_Scroll(object sender, EventArgs e)
+        {
+
         }
     }
 }
